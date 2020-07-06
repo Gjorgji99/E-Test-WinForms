6c2fe990faeb6c47185bfb449fb7cbc318556762
Ѓузелов Ѓорѓи <gjuzelovgjorgji@gmail.com>
2020-07-06 13:56:19 +0200

    First release

---
+++ WindowsFormsApp1/Prasanje.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Prasanje
+    {
+        string naslov;
+        string[] ponudi;
+        string tocen;
+
+        public Prasanje()
+        {
+            ponudi = new string[5];
+        }
+
+        public Prasanje(string naslov, string[] ponudi, string tocen)
+        {
+            this.naslov = naslov;
+            this.ponudi = ponudi;
+            this.tocen = tocen;
+        }
+
+        public string Naslov { get => naslov; set => naslov = value; }
+        public string[] Ponudi { get => ponudi; set => ponudi = value; }
+        public string Tocen { get => tocen; set => tocen = value; }
+
+    }
+}
---
+++ WindowsFormsApp1/testform.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Testform : Form
+    {
+        public Testform()
+        {
+            InitializeComponent();
+        }
+        private string ime;
+        private List<Prasanje> prasanja =  new List<Prasanje> { };
+        int points = 0;
+        int count = 0;
+        Random index = new Random();
+
+        public void setIme(string ime)
+        {
+            this.ime = ime;
+        }
+
+        protected void testform_Load(object sender, EventArgs e)
+        {
+            lname.Text += ime;
+            string filePath = Path.Combine(Environment.CurrentDirectory, "\\Prasanja1.txt");
+            string[] lines = File.ReadAllLines(filePath);
+            for(int i = 0; i < lines.Length; i++)
+            {
+                string[] parts = lines[i].Split(',');
+                List<string> odgovor = new List<string> {};
+                odgovor.Add(parts[1]);
+                odgovor.Add(parts[2]);
+                odgovor.Add(parts[3]);
+                odgovor.Add(parts[4]);
+                prasanja.Add(new Prasanje(parts[0], odgovor.ToArray(), parts[5]));
+            }
+            
+            load();
+            
+        }
+        private void load()
+        {
+            int i = index.Next(0, prasanja.Count);
+            lprasanje.Text = "Прашање: " + prasanja[i].Naslov;
+            radioButton1.Text = prasanja[i].Ponudi[0];
+            radioButton2.Text = prasanja[i].Ponudi[1];
+            radioButton3.Text = prasanja[i].Ponudi[2];
+            radioButton4.Text = prasanja[i].Ponudi[3];
+            if(prasanja[i].Tocen == Radiocheck1())
+            {
+                points++;
+            }
+            count++;
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private string Radiocheck1()
+        {
+            
+                if (radioButton1.Checked)
+            {
+                return radioButton1.Text;
+            }
+            if (radioButton2.Checked)
+            {
+                return radioButton2.Text;
+            }
+            if (radioButton3.Checked)
+            {
+                return radioButton3.Text;
+            }
+            
+            return radioButton4.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(count == 3)
+            {
+                load();
+                button1.Text = "Заврши";
+                this.Hide();
+                MessageBox.Show("Точни прашања: " + points);
+            }
+            else
+            {
+                load();
+            }
+        }
+    }
+}
