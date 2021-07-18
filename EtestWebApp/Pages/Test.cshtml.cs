343e058ed45af1fed51f2498afadfc2a054ab4ad
Gjorgji99 <gjuzelovgjorgji@gmail.com>
2021-07-18 12:27:05 +0200

    Big Update Bug Fixes

--- EtestWebApp/Pages/Test.cshtml.cs
+++ EtestWebApp/Pages/Test.cshtml.cs
@@ -14,8 +14,9 @@ namespace EtestWebApp.Pages
 {
     public class TestModel : PageModel
     {
-        public User user = new User();
+        public User user;
         public string buttonNext = "Next"; 
+        public bool buttonBack = true;
         public Question question;
 
         public TestModel()
@@ -25,31 +26,44 @@ namespace EtestWebApp.Pages
         public void OnGet(){
             this.user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("SessionUser"));
             ViewData["StudentName"] = user.Name;
-             question = user.Test.Load;
+            question = user.Test.Load;
+            HttpContext.Session.SetString("SessionUser",JsonConvert.SerializeObject(this.user));
+
         }
         public IActionResult OnPostNext(string answer)
         {
             this.user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("SessionUser"));
+            if(1-user.Test.Count >0)
+                buttonBack = false;
+            else{
+                buttonBack=true;
+            }
             if(user.Test.Count==user.Test.NumberOfQuestions-2){
                 buttonNext = "Finish";
             }
             question = user.Test.Next(answer);
-            HttpContext.Session.SetString("SessionUser",JsonConvert.SerializeObject(user));
-           
+            HttpContext.Session.SetString("SessionUser",JsonConvert.SerializeObject(this.user));
             return Page();
         }
         public IActionResult OnPostBack(){
             this.user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("SessionUser"));
+            buttonNext = "Next";
+            if(1-user.Test.Count > 0)
+                buttonBack = false;
+            else{
+                buttonBack=true;
+            }
             question = user.Test.Back();
             ViewData["StudentName"] = user.Name;
-            HttpContext.Session.SetString("SessionUser",JsonConvert.SerializeObject(user));
+            HttpContext.Session.SetString("SessionUser",JsonConvert.SerializeObject(this.user));
             return Page();
         }
         public IActionResult OnPostFinish(string answer){
+            buttonBack = false;
             this.user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString("SessionUser"));
             user.Test.checkQuestion(answer);
             ViewData["StudentName"] = user.Name;
-            HttpContext.Session.SetString("SessionUser",JsonConvert.SerializeObject(user));
+            HttpContext.Session.SetString("SessionUser",JsonConvert.SerializeObject(this.user));
             return RedirectToPage("/Result");
         }
 
